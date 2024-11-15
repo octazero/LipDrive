e7bc88336f91ed718f80f503054a85aeef0ce685
Amr Youssef <amrt.nabih@gmail.com>
2024-11-15 19:17:32 +0100

    init

--- LIPDRIVE/LipDrive/Views/PagesCollectionViewCell.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-//This class is used for the collectionview (The paging tabs)
-class PagesCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var PageUIView: UIView!
-}
--- LIPDRIVE/Pods/Target Support Files/Pods-LipDriveUITests/Pods-LipDriveUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LipDriveUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LipDriveUITests
-@end
